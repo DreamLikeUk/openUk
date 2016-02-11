be93cb1bb51df1d803eb3852048ce0abd3346631
Stacy <stace1995D@gmail.com>
2016-02-12 01:12:36 +0200

    services go here

---
+++ src/main/java/com/dream/like/uk/services/IUserService.java
@@ -0,0 +1,46 @@
+package com.dream.like.uk.services;
+
+import com.dream.like.uk.domain.entities.UserEntity;
+import com.dream.like.uk.domain.enums.RoleEnum;
+import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion;
+
+/**
+ * Created by Stacy on 2/12/16.
+ */
+public interface IUserService {
+    /**
+     * Returns the user, which corresponds to given id
+     * @param userId
+     * @return
+     */
+    UserEntity getUserById(int userId);
+
+    /**
+     * Returns the user, which corresponds to given mail
+     * @param mail
+     * @return
+     */
+    UserEntity getUserByMail(String mail);
+
+    /**
+     * Creates or updates the user
+     * @param user
+     * @return
+     */
+    UserEntity updateUser(UserEntity user);
+
+    /**
+     * Delete user, which corresponds to given id
+     * @param userId
+     * @return
+     */
+    boolean deleteUser(int userId);
+
+    /**
+     * Returns role of the given user
+     * @param userEntity
+     * @return
+     */
+    RoleEnum getUserRole(UserEntity userEntity);
+
+}
