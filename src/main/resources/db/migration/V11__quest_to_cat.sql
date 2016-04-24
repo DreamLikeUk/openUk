9b3c3179e1128aad60663e133c02b827cd2528be
Natalia <nataliakiselyk@gmail.com>
2016-04-25 02:40:29 +0300

    migration edited

--- src/main/resources/db/migration/V11__quest_to_cat.sql
+++ src/main/resources/db/migration/V11__quest_to_cat.sql
@@ -3,5 +3,5 @@ CHANGE COLUMN `text` `text` VARCHAR(300) NOT NULL COMMENT '' ;
 
 INSERT INTO `openUk`.`questions` (`id_category`, `text`, `points`) VALUES ('1', 'Правильно написано всі слова у рядку', '1');
 INSERT INTO `openUk`.`questions` (`id_category`, `text`, `points`) VALUES ('1', 'Апостроф потрібно  вставити у всіх рядках КРІМ', '1');
-INSERT INTO `openUk`.`questions` (`id_category`, `text`, `points`) VALUES ('1', 'М’який знак потрібено вставити у рядку', '1');
+INSERT INTO `openUk`.`questions` (`id_category`, `text`, `points`) VALUES ('1', 'М’який знак потрібно вставити у рядку', '1');
 INSERT INTO `openUk`.`questions` (`id_category`, `text`, `points`) VALUES ('1', 'Правильно написані всі слова у рядку', '1');
