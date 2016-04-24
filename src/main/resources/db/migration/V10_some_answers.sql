e6cebda7890c3250e4aa1d8bbc758dabdc30dad4
Natalia <nataliakiselyk@gmail.com>
2016-04-25 02:36:45 +0300

    added migrations

---
+++ src/main/resources/db/migration/V10_some_answers.sql
@@ -0,0 +1,17 @@
+INSERT INTO `openUk`.`answers` (`id_question`, `text`, `correct`) VALUES ('6', 'спрясти, сгребти, припогано', '0');
+INSERT INTO `openUk`.`answers` (`id_question`, `text`, `correct`) VALUES ('6', 'притулок, прикордонний, схватити', '1');
+INSERT INTO `openUk`.`answers` (`id_question`, `text`, `correct`) VALUES ('6', 'росхитати, надпотужний, понатплановий', '0');
+
+INSERT INTO `openUk`.`answers` (`id_question`, `text`, `correct`) VALUES ('7', 'Лук...ян, різдв...яний, ад...ютант', '0');
+INSERT INTO `openUk`.`answers` (`id_question`, `text`, `correct`) VALUES ('7', 'рив...єра, б...юмін, моркв...яний', '0');
+INSERT INTO `openUk`.`answers` (`id_question`, `text`, `correct`) VALUES ('7', 'Мар...яна, кон...юнктура, верб...я', '1');
+
+INSERT INTO `openUk`.`answers` (`id_question`, `text`, `correct`) VALUES ('8', 'Хар...ків, нен...ці, батал...йон', '0');
+INSERT INTO `openUk`.`answers` (`id_question`, `text`, `correct`) VALUES ('8', 'Тан...чин, стан...ся, ател...є', '1');
+INSERT INTO `openUk`.`answers` (`id_question`, `text`, `correct`) VALUES ('8', 'воротар..., Гот...є, конферанс...є', '0');
+
+INSERT INTO `openUk`.`answers` (`id_question`, `text`, `correct`) VALUES ('10', 'однорідний, велодоріжка,  біо-йогурт', '0');
+INSERT INTO `openUk`.`answers` (`id_question`, `text`, `correct`) VALUES ('10', 'чотирьохактний, шістдесятирічний, сільськогосподарський', '1');
+INSERT INTO `openUk`.`answers` (`id_question`, `text`, `correct`) VALUES ('10', 'пів-місяць, людино-день, норд-вест', '0');
+
+
---
+++ src/main/resources/db/migration/V11_quest_to_cat.sql
@@ -0,0 +1,7 @@
+ALTER TABLE `openUk`.`questions` 
+CHANGE COLUMN `text` `text` VARCHAR(300) NOT NULL COMMENT '' ;
+
+INSERT INTO `openUk`.`questions` (`id_category`, `text`, `points`) VALUES ('1', 'Правильно написано всі слова у рядку', '1');
+INSERT INTO `openUk`.`questions` (`id_category`, `text`, `points`) VALUES ('1', 'Апостроф потрібно  вставити у всіх рядках КРІМ', '1');
+INSERT INTO `openUk`.`questions` (`id_category`, `text`, `points`) VALUES ('1', 'М’який знак потрібено вставити у рядку', '1');
+INSERT INTO `openUk`.`questions` (`id_category`, `text`, `points`) VALUES ('1', 'Правильно написані всі слова у рядку', '1');
